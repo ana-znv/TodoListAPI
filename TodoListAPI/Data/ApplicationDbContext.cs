c99eeea3dcfd555860a1f783c2f3c52a5ce64d52
Vilgefortz13 <rinatuzanzaev0@gmail.com>
2025-05-23 22:55:46 +0600

    added database

---
+++ TodoListAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListAPI.Models.Entities;
+
+namespace TodoListAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
---
+++ TodoListAPI/Models/Entities/Todo.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoListAPI.Models.Entities
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDone { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+    }
+}
